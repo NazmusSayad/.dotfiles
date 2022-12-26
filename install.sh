9385f2b048ba044525d399cf5d714ab731248c4b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-12-27 00:52:14 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-cp ./scripts ~/temp-custom-scripts -r
 cp ./lib/.gitignore ~/.gitignore
-
 cat ./scripts/main.sh > ~/.bashrc
-echo >> ~/.bashrc
-cat ./scripts/node.sh >> ~/.bashrc
 
 git config --global core.excludesfile ~/.gitignore
 
