5b6a05aa6bdd38fef0d331ddd1b0700fbb052bfa
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-12-27 00:38:49 +0600

    Update node.sh

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,11 +1,10 @@
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-rm ~/.nodejs /opt/nodejs -rf
-nvm install node --use
-nvm alias default node
-
 cat ~/temp-custom-scripts/main.sh > ~/.bashrc
 rm ~/temp-custom-scripts -rf
 
+rm ~/.nodejs /opt/nodejs -rf
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install node --use
+nvm alias default node
 npm i -g npm@latest live-server gitignore
 
 clear
