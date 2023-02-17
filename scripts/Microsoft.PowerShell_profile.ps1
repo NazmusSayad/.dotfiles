2ce2d89f4dbeb603d8eccf3e48a74566450c764b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-02-17 09:08:16 +0600

    Update Microsoft.PowerShell_profile.ps1

--- scripts/Microsoft.PowerShell_profile.ps1
+++ scripts/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,8 @@ Function ghp{param($p);if($p.Length -eq 0){$p=Read-Host "#"}if($p -eq ''){$p="..
 Function ghi{Remove-Item .git -Force -Recurse;cls;git init;git add .;git commit -m "init..."}
 Function df{param($p);If(([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){Defrag.exe /C;if($p -eq 'r'){shutdown.exe -r -t 0}elseif($p -eq 's'){shutdown.exe -s -t 0}}}
 Function ghhr{param($branch);if($branch.Length -eq 0){$branch="master"};git checkout --orphan latest_branch;git add -A;git commit -am "init...";git branch -D $branch;git branch -m $branch;git push -f origin $branch;}
-Function port{Param([Parameter(Mandatory=$true)][string]$from,[Parameter(Mandatory=$true)][string]$to);netsh interface portproxy reset;netsh interface portproxy add v4tov4 listenport=$to connectport=$from connectaddress=127.0.0.1}
+Function port-reset{netsh interface portproxy reset}
+Function port{Param([Parameter(Mandatory=$true)][string]$from,[Parameter(Mandatory=$true)][string]$to);netsh interface portproxy add v4tov4 listenport=$to connectport=$from connectaddress=127.0.0.1}
 
 Set-Alias -Name y -Value yarn
 cls;
