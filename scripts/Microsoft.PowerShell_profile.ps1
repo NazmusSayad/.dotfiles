a7bb875999f4b0cf1191188f261519cfb1e79ef9
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-24 08:51:32 -0700

    idk

--- scripts/Microsoft.PowerShell_profile.ps1
+++ scripts/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,68 @@
-Function ghc{param($p);gh repo clone $p}
-Function ghp{param($p);if($p.Length -eq 0){$p=Read-Host "#"}if($p -eq ''){$p="..."}git status;git add .;git commit -m $p;git push}
-Function ghi{Remove-Item .git -Force -Recurse;cls;git init;git add .;git commit -m "init..."}
-Function df{param($p);If(([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){Defrag.exe /C;if($p -eq 'r'){shutdown.exe -r -t 0}elseif($p -eq 's'){shutdown.exe -s -t 0}}}
-Function ghhr{param($branch);if($branch.Length -eq 0){$branch="master"};git checkout --orphan latest_branch;git add -A;git commit -am "init...";git branch -D $branch;git branch -m $branch;git push -f origin $branch;}
-Function port-reset{netsh interface portproxy reset}
-Function port{Param([Parameter(Mandatory=$true)][string]$from,[Parameter(Mandatory=$true)][string]$to);netsh interface portproxy add v4tov4 listenport=$to connectport=$from connectaddress=127.0.0.1}
-
+# Alias
 Set-Alias -Name y -Value yarn
-cls;
+Set-Alias -Name live -Value live-server
+function ya {iex "yarn add $($args)"}  
+function yad {iex "yarn add -D $($args)"}
+function n {iex "node $($args)"}
+function nw {iex "node --watch $($args)"}
+function tn {iex "ts-node $($args)"}
+function tnw {iex "ts-node-dev $($args)"}
+function nm {iex "nodemon $($args)"}
+
+# Functions
+function ghp {
+  $msg=($args -join " ").Trim()
+  if ([string]::IsNullOrEmpty($msg)) {
+    Write-Host "You must need to give a <commit_message>" -ForegroundColor Red
+    return
+  }
+  
+  git status --short
+  Write-Host ""
+
+  git add -A | Out-Null
+  git commit -m "$msg"
+  Write-Host ""
+
+  git push --quiet
+}
+
+function ghr {
+  param (
+    [string]$hash
+  )
+
+  if ([string]::IsNullOrEmpty($hash)) {
+    Write-Host "You must need to give a <commit_id>" -ForegroundColor Red
+    return
+  }
+
+  git checkout $hash . 
+  ghp ("$hash restored; " + ($args[1..($args.Length - 1)] -join " "))
+} 
+
+function ghhr {
+  param (
+    [string]$branch = "master"
+  )
+
+  git checkout --orphan latest_branch
+  git add -A
+  git commit -am "initial commit"
+  git branch -D $branch
+  git branch -m $branch
+  git push -f origin $branch
+}
+
+function ghc {
+  param (
+    [string]$repo
+  )
+
+  if ($repo -match "^http.*") {
+    git clone $repo
+  }
+  else {
+    gh repo clone $repo
+  }
+}
