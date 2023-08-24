a652240bb0acc0a01aa85ac42fb839ccd989901b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-24 14:51:15 -0700

    D config  D install.ps1  M install.sh  D lib/.gitignore  D scripts/.bashrc  D scripts/Microsoft.PowerShell_profile.ps1 ?? .bashrc ?? default.gitignore

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-cat ./scripts/.bashrc > ~/.bashrc
+cat ./.bashrc > ~/.bashrc
 
-cp ./lib/.gitignore ~/.gitignore
-git config --global core.excludesfile ~/.gitignore
+cp ./default.gitignore ~/default.gitignore
+git config --global core.excludesfile ~/default.gitignore
+echo "Default gitignore added"
 
-mv ./config ./config.sh
-source config.sh
-mv ./config.sh ./config
+git config --global user.name "Nazmus Sayad"
+git config --global user.email "87106526+NazmusSayad@users.noreply.github.com"
+echo "Git auth info added"
+
+git config --global init.defaultBranch master
+git config --global --add --bool push.autoSetupRemote true
+echo "Git config added..."
+
+npm config set ignore-scripts true
+echo "Npm config added..."
+
+npm i -g npm yarn nodemon ts-node ts-node-dev live-server gitignore code-info
+echo "Npm packages installed..."
+
+source ./.bashrc
