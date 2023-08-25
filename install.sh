261db3f5e9b5de84e887ad3106b56be0e823f32b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-25 12:24:56 +0600

    M install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ echo "Git config added..."
 npm config set ignore-scripts true
 echo "Npm config added..."
 
-npm i -g npm yarn nodemon ts-node ts-node-dev live-server gitignore code-info
+npm i -g npm yarn nodemon ts-node ts-node-dev live-server gitignore code-info yta
 echo "Npm packages installed..."
 
 source ./.bashrc
