296e2b038920937ca677280e838f40d7788088de
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-09-28 15:08:08 +0600

    M scripts/fish_theme.fish  D test.fish

--- scripts/fish_theme.fish
+++ scripts/fish_theme.fish
@@ -1,13 +1,13 @@
 function __fish_shell_path
     set dir (pwd)
-    if test "$OSTYPE" = cygwin -o "$OSTYPE" = msys
+    if test "$OS" = Windows_NT
         set win_dir $dir
         set first_part (string sub -s 2 -l 1 $win_dir)
         set first_part_uppercase (string upper $first_part)
         set second_part (string sub -s 4 $win_dir)
         set dir "$first_part_uppercase: $second_part"
     end
-    echo $dir
+    echo -n $dir
 end
 
 function __fish_git_branch
@@ -15,42 +15,37 @@ function __fish_git_branch
         return
     end
 
-    set branch (git symbolic-ref --short HEAD 2>/dev/null; or echo (git rev-parse --short HEAD 2>/dev/null))
-    set output "$branch"
-    echo -n "$branch"
+    echo -n (git symbolic-ref --short HEAD 2>/dev/null; or echo (git rev-parse --short HEAD 2>/dev/null))
 end
 
 function __fish_git_changes
     if not git diff --quiet; or not git diff --cached --quiet
-        set output "$output ✗"
+        echo -n "✘"
     end
 
     if test (git log --branches --not --remotes | wc -l) -gt 0
-        set output "$output ⬆"
+        echo -n "➜"
     end
-
-    set output "$output"
-    echo -n "$output"
 end
 
 function fish_prompt
     set_color magenta
     echo -n '⌠ '
-    set_color blue
+    set_color cyan
     echo -n (__fish_shell_path)
     set_color normal
 
     set branch_output (__fish_git_branch)
     if test -n "$branch_output"
         echo -n ' ('
-        set_color yellow
+        set_color red
         echo -n "$branch_output"
         set_color normal
 
         set changes_output (__fish_git_changes)
         if test -n $changes_output
-            set_color red
-            echo -n $changes_output
+            set_color yellow
+            echo -n " $changes_output"
             set_color normal
         end
 
--- test.fish
+++
@@ -1,5 +0,0 @@
-if not git diff --quiet; or not git diff --cached --quiet
-  echo "Has uncommitted changes"
-else
-  echo "No uncommitted changes"
-end
