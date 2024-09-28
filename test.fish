b83e231bc33acba9f7947d9d1453873ba254205e
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-09-28 14:47:37 +0600

    Fish shell added

--- test.fish
+++ test.fish
@@ -1,4 +1,5 @@
-if test (git log --branches --not --remotes | wc -l) -gt 0
-  echo "Has changes"
+if not git diff --quiet; or not git diff --cached --quiet
+  echo "Has uncommitted changes"
+else
+  echo "No uncommitted changes"
 end
-
