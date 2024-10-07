0a4861979647b2de06058be5cfb7751b1dc7d6c3
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-10-07 21:27:13 +0600

    M install.sh ?? install-sh.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-__dirname="$(dirname "$(realpath "$0")")"
-echo "source $__dirname/bash-config/__init__.sh" >~/.bashrc
-echo "Bash config linked"
-
-echo "source $__dirname/fish-config/__init__.fish" >~/.config/fish/config.fish
-echo "Fish config linked"
+source ./install-sh.sh
 
 cp ./lib/default.gitignore ~/default.gitignore -f
 echo "Default gitignore added"
