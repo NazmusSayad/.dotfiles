144c6c3e085b0d485d9b54f3fbbb3c95358c8814
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-05-07 02:13:20 +0600

    fix: improve branch deletion confirmation output in gbc function

--- fish-config/functions.fish
+++ fish-config/functions.fish
@@ -3,17 +3,34 @@ function gbc
     set branches (git branch --format="%(refname:short)" | grep -v $current)
 
     if test (count $branches) -gt 0
-        echo "The following branches will be deleted:"
-        echo $branches | tr ' ' '\n'
-        echo -n "Press [Enter] to confirm, or any other key to cancel: "
+        set_color --dim
+        echo -n "Branches to delete: "
+        set_color normal
 
-        read -l -n 1 confirm
-        if test -z "$confirm"
-            git branch -d $branches
-        else
-            echo "❌  Cancelled branch deletion."
+        set_color --bold red
+        echo (string join ', ' $branches)
+        set_color normal
+
+        echo "Press [Enter] to confirm, or any other key to cancel: "
+        read -n 1 confirm
+        set read_status $status
+
+        if test $read_status -ne 0
+            set_color green
+            echo "Cancelled branch deletion."
+            set_color normal
             return 1
         end
+
+        if test -z "$confirm"
+            set_color --dim red
+            git branch -d $branches
+        else
+            set_color green
+            echo "Cancelled branch deletion."
+        end
+
+        set_color normal
     else
         echo "No other branches to delete"
     end
