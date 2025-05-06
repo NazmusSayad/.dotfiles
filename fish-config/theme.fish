4359049b4bdcb337724c253d78ec9b132bbed92b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-05-07 02:56:56 +0600

    Minor fixes

--- fish-config/theme.fish
+++ fish-config/theme.fish
@@ -22,7 +22,7 @@ function __fish_git_changes
     if not git diff --quiet --cached || not git diff --quiet || git ls-files --others --exclude-standard | grep -q "."
         echo -n "✘"
     else if git log --branches --not --remotes | grep -q "."
-        echo -n "✚"
+        echo -n "✔"
     end
 end
 
