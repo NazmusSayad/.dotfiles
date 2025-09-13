c8d871a999eba9d5d06519757b5991d8d7b1c62f
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-09-14 04:23:10 +0600

    Enhance compile.cmd output for better user feedback during builds
    - Updated echo statements to provide clearer progress messages for compiling ahk scripts, winget-install, winget-upgrade, and clean-code-snippets.
    - Ensured consistent formatting in command outputs to improve user experience.

--- compile.cmd
+++ compile.cmd
@@ -1,15 +1,20 @@
 @echo off
 
 echo.
-echo Compiling ahk scripts
+echo ^> Compiling ahk scripts...
 call npm run ahk-compile
 
 echo.
-echo Compiling clean-code-snippets
-call go build -o ./___winget-install.exe ./src/winget/install/main.go
-call go build -o ./___winget-upgrade.exe ./src/winget/upgrade/main.go
-call go build -o ./___clean-vscode-snippets.exe ./src/clean-code-snippets/main.go
+echo ^> Compiling winget-install...
+call go build -o .\___winget-install.exe .\src\winget\install\main.go
 
 echo.
+echo ^> Compiling winget-upgrade...
+call go build -o .\___winget-upgrade.exe .\src\winget\upgrade\main.go
+
+echo.
+echo ^> Compiling clean-code-snippets...
+call go build -o .\___clean-vscode-snippets.exe .\src\clean-code-snippets\main.go
+
 echo.
 pause
