a3b46bd43f64c506d57add5e633c153589e1a038
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-10-21 16:12:08 +0600

    Refactor npm aliases in fish configuration for improved clarity and consistency

--- config/fish-config/aliases.fish
+++ config/fish-config/aliases.fish
@@ -1,16 +1,20 @@
+# GitHub CLI Aliases
 alias c="gh repo clone"
 alias gv="gh repo view"
 alias gw="gh repo view --web"
 
+# Git Aliases
 alias gp="git pull"
 alias gb="git branch"
 alias gc="git checkout"
 alias gcb="git checkout -b"
 
-alias r="npm run"
+# NPM Aliases
+alias n="npm run"
 alias nx="npm exec"
 alias np="npm prune"
+alias na="npm add --save"
+alias nad="npm add --save-dev"
+alias nr="npm remove --save"
+alias nu="npm update --save"
 alias ni="npm install --save"
-alias nid="npm install --save-dev"
-alias nu="npm uninstall --save"
-alias nup="npm update --save"
