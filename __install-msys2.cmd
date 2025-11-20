35e7af5f40acd94802c82b4ef5fbd7a04ef5dfbc
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-11-20 22:08:57 +0600

    Update __install-msys2.cmd to install additional helpers and correct package names; add cursor hooks configuration to symlink.jsonc for improved user experience.

--- __install-msys2.cmd
+++ __install-msys2.cmd
@@ -5,7 +5,10 @@ echo "> Installing shell providers..."
 pacman -S zsh fish bash
 
 echo "> Installing shell helpers..."
-pacman -S mingw-w64-ucrt-x86_64-starship mingw-w64-clang-x86_64-fastfetch
+pacman -S mingw-w64-x86_64-starship mingw-w64-clang-x86_64-fastfetch
+
+echo "> Installing helpers..."
+pacman -S mingw-w64-x86_64-ffmpeg
 
 echo.
 echo "> Press any key to continue..."
