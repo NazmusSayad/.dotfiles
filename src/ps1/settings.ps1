4f68915e4c866db5a19bd9701a021f67890e0ced
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-01 13:26:56 +0600

    Update windows-terminal.json to add minimizeToNotificationArea option and set startingDirectory to null for Command Prompt and Windows PowerShell. Modify windowingBehavior to useNew. Remove Ultimate Performance power plan commands from settings.ps1 for cleanup.

--- src/ps1/settings.ps1
+++ src/ps1/settings.ps1
@@ -5,7 +5,3 @@ Remove-Item "C:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Progr
 Remove-Item "C:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.exe" -ErrorAction Continue
 Remove-Item "C:\Windows\System32\OneDriveSetup.exe" -ErrorAction Continue
 Remove-Item "C:\Windows\SysWOW64\OneDriveSetup.exe" -ErrorAction Continue
-
-# Enable the Ultimate Performance power plan
-powercfg.exe -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
-powercfg.exe -setactive e9a42b02-d5df-448d-aa00-03f14749eb61
