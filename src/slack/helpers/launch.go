834e919ae1875cc84110b7195d092ad479bd095b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-05 03:36:33 +0600

    Remove __compile.cmd script and refactor Slack launch logic in launch.go to improve time zone handling and work hour checks.

--- src/slack/helpers/launch.go
+++ src/slack/helpers/launch.go
@@ -4,22 +4,34 @@ import (
 	"time"
 )
 
-var workTimeStart = time.Date(2025, 12, 5, 9, 0, 0, 0, time.Local)
-var workTimeEnd = time.Date(2025, 12, 5, 17, 0, 0, 0, time.Local)
+var bangladeshTZ = time.FixedZone("GMT+6", 6*60*60)
+
+func isWorkTime() bool {
+	now := time.Now().In(bangladeshTZ)
+	weekday := now.Weekday()
+	hour := now.Hour()
+
+	if weekday == time.Friday || weekday == time.Saturday {
+		return false
+	}
+
+	return hour >= 6 && hour < 20
+}
 
 func SlackLaunch(status string) {
-	if status == "always" {
+	switch status {
+	case "always":
 		SlackApplicationStart()
-	} else if status == "never" {
+
+	case "never":
 		SlackApplicationStop()
-	} else if status == "work-hours" {
-		currentTime := time.Now()
 
-		println("Current time:", currentTime.Format("2006-01-02 15:04:05"))
-		println("Work time start:", workTimeStart.Format("2006-01-02 15:04:05"))
-		println("Work time end:", workTimeEnd.Format("2006-01-02 15:04:05"))
+	case "work-hours":
+		now := time.Now().In(bangladeshTZ)
+		println("Current time (BD):", now.Format("2006-01-02 15:04:05 Mon"))
+		println("Work hours: 6AM-8PM, Off: Fri/Sat")
 
-		if currentTime.After(workTimeStart) && currentTime.Before(workTimeEnd) {
+		if isWorkTime() {
 			println("Starting Slack...")
 			SlackApplicationStart()
 		} else {
