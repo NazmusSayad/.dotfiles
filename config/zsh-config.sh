5811d9eb7200bf866b7096ce1ecf1c6212a57f13
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-04 12:27:59 +0600

    Refactor symlink.jsonc to swap zsh and fish configurations, add new script symlinks for bash and zsh. Clean up winget-apps.jsonc by removing Bun entry. Update zsh-config.sh to source additional zsh completions and suggestions for enhanced functionality.

--- config/zsh-config.sh
+++ config/zsh-config.sh
@@ -1,1 +1,4 @@
 eval "$(starship init zsh)"
+source ~/.config/scripts/zsh/zsh-completions/zsh-completions.plugin.zsh
+source ~/.config/scripts/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/scripts/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
