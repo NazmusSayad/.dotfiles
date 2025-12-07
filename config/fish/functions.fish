e6fa7296f815c37cba41cdb52bf827da0d0655f7
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-07 19:15:12 +0600

    Refactor fish functions for Git: add 'gr' for restore and clean, 'gp' for pulling changes (default), and 'gpr' for pulling changes (rebase). Introduce 'gbclean' for branch cleanup with confirmation prompt. Remove obsolete 'gbc' and 'gr' functions to streamline code.

--- config/fish/functions.fish
+++ config/fish/functions.fish
@@ -26,106 +26,6 @@ function c
     end
 end
 
-# Git Branch Cleanup
-function gbc
-    set current (git branch --show-current)
-    set branches (git branch --format="%(refname:short)" | grep -v $current)
-
-    if test (count $branches) -gt 0
-        echo -n "Branches to delete: "
-
-        set_color --bold red
-        echo (string join ', ' $branches)
-        set_color normal
-
-        set_color normal
-        set_color --dim
-        echo -n "Press [Enter] to confirm, or any other key to cancel: "
-        set_color normal
-
-        read -n 1 -P "" --function confirm
-
-        if test $status -eq 0 -a -z "$confirm"
-            set_color --dim red
-            git prune --progress
-            git branch -D $branches
-        else
-            set_color green
-            echo "Cancelled branch deletion"
-        end
-
-        set_color normal
-    else
-        echo "No other branches to delete"
-    end
-end
-
-# Git Reset
-function greset
-    set_color red
-    echo "This will reset the entire repository to the latest remote branch."
-    set_color normal
-
-    echo "Write 'yes' and press [Enter] to confirm."
-    read -P "> " confirm
-
-    if test "$confirm" != yes
-        set_color green
-        echo "Reset aborted"
-        set_color normal
-        return 0
-    end
-
-    git fetch --all
-
-    set --local remote_url (git remote get-url origin)
-    set --local current_branch (git branch --show-current)
-
-    echo -en "> Branch: "
-    set_color yellow
-    echo -e "$current_branch"
-    set_color normal
-
-    echo -en "> Remote: "
-    set_color blue
-    echo -e "$remote_url"
-    set_color normal
-
-    set --local remote_branches (git branch -r --format="%(refname:short)" | string split '\n')
-    for rb in $remote_branches
-        set --local match (string match -r '.+/.+' $rb)
-        if test -z "$match"
-            continue
-        end
-
-        set --local rb (string replace -r '^[^/]*/' '' $rb)
-        if test "$rb" = "$current_branch"
-            continue
-        end
-
-        echo -en "> Deleting remote branch: "
-        set_color red
-        echo -e "$rb"
-        set_color normal
-
-        git push origin --delete $rb
-    end
-
-    set_color red
-    echo '> Deleting git folder...'
-    set_color normal
-
-    rm -rf .git
-
-    git init --initial-branch=$current_branch
-    git remote add origin $remote_url
-
-    git add .
-    git commit -m "Initial commit"
-    git push --force --set-upstream origin $current_branch
-
-end
-
 # Git Restore
 function gr
     set_color red
@@ -211,6 +111,106 @@ function gpr
 
     git prune --progress
     git pull origin $target_branch --progress --rebase
+end
+
+# Git Reset
+function greset
+    set_color red
+    echo "This will reset the entire repository to the latest remote branch."
+    set_color normal
+
+    echo "Write 'yes' and press [Enter] to confirm."
+    read -P "> " confirm
+
+    if test "$confirm" != yes
+        set_color green
+        echo "Reset aborted"
+        set_color normal
+        return 0
+    end
+
+    git fetch --all
+
+    set --local remote_url (git remote get-url origin)
+    set --local current_branch (git branch --show-current)
+
+    echo -en "> Branch: "
+    set_color yellow
+    echo -e "$current_branch"
+    set_color normal
+
+    echo -en "> Remote: "
+    set_color blue
+    echo -e "$remote_url"
+    set_color normal
+
+    set --local remote_branches (git branch -r --format="%(refname:short)" | string split '\n')
+    for rb in $remote_branches
+        set --local match (string match -r '.+/.+' $rb)
+        if test -z "$match"
+            continue
+        end
+
+        set --local rb (string replace -r '^[^/]*/' '' $rb)
+        if test "$rb" = "$current_branch"
+            continue
+        end
+
+        echo -en "> Deleting remote branch: "
+        set_color red
+        echo -e "$rb"
+        set_color normal
+
+        git push origin --delete $rb
+    end
+
+    set_color red
+    echo '> Deleting git folder...'
+    set_color normal
+
+    rm -rf .git
+
+    git init --initial-branch=$current_branch
+    git remote add origin $remote_url
+
+    git add .
+    git commit -m "Initial commit"
+    git push --force --set-upstream origin $current_branch
+
+end
+
+# Git Branch Cleanup
+function gbclean
+    set current (git branch --show-current)
+    set branches (git branch --format="%(refname:short)" | grep -v $current)
+
+    if test (count $branches) -gt 0
+        echo -n "Branches to delete: "
+
+        set_color --bold red
+        echo (string join ', ' $branches)
+        set_color normal
+
+        set_color normal
+        set_color --dim
+        echo -n "Press [Enter] to confirm, or any other key to cancel: "
+        set_color normal
+
+        read -n 1 -P "" --function confirm
+
+        if test $status -eq 0 -a -z "$confirm"
+            set_color --dim red
+            git prune --progress
+            git branch -D $branches
+        else
+            set_color green
+            echo "Cancelled branch deletion"
+        end
+
+        set_color normal
+    else
+        echo "No other branches to delete"
+    end
 end
 
 # GPG Unlock
