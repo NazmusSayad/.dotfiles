afb08e7b34606188359f07704842d2c09e3e0ef2
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-12 14:50:34 +0600

    Refactor __compile.cmd to streamline build process
    Added steps to kill all AHK scripts and clean the build directory before compilation, improving the overall efficiency of the build process. Removed redundant AHK script termination step from the compilation sequence.

--- __compile.cmd
+++ __compile.cmd
@@ -1,15 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo ^> Killing all AHK scripts...
+sudo taskkill /F /IM AHK-*
+
+echo ^> Cleaning build directory...
+rmdir /S /Q ./build
+
 echo ^> Compiling Go scripts...
 go run ./src/compile-scripts/main.go
 
 echo.
 echo ^> Compiling Rust functions...
 go run ./src/compile-functions/main.go
-
-echo ^> Killing all AHK scripts...
-sudo taskkill /F /IM AHK-*
 
 echo.
 echo ^> Compiling AutoHotkey scripts...
