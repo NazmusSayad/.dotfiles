ad36d4c794c36cbe2f3544224f2bc40139ee63a9
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-25 02:25:18 +0600

    Update __install-go-tools.cmd: change PowerShell command execution to use 'call' for setting Go environment variables, ensuring proper execution in batch scripts.

--- __install-go-tools.cmd
+++ __install-go-tools.cmd
@@ -2,8 +2,8 @@
 setlocal
 
 echo ^> Setting up Go environment variables...
-powershell.exe -NoProfile -Command "[Environment]::SetEnvironmentVariable('GOPATH', (go env GOPATH), 'User')"
-powershell.exe -NoProfile -Command "[Environment]::SetEnvironmentVariable('GOROOT', (go env GOROOT), 'User')"
+call powershell.exe -NoProfile -Command "[Environment]::SetEnvironmentVariable('GOPATH', (go env GOPATH), 'User')"
+call powershell.exe -NoProfile -Command "[Environment]::SetEnvironmentVariable('GOROOT', (go env GOROOT), 'User')"
 
 echo ^> Installing Go tools...
 call go install -v mvdan.cc/sh/v3/cmd/shfmt@latest
