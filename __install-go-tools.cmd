1cd4254ae30171bb90618f5a973ccf614862f837
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-25 02:02:40 +0600

    Update __install-go-tools.cmd: set up Go environment variables and install essential Go tools for improved development setup.

--- __install-go-tools.cmd
+++ __install-go-tools.cmd
@@ -1,7 +1,11 @@
 @echo off
 setlocal
 
-echo ^> Installing go...
+echo ^> Setting up Go environment variables...
+for /f "usebackq delims=" %%G in (`go env GOPATH`) do setx GOPATH "%%G"
+for /f "usebackq delims=" %%G in (`go env GOROOT`) do setx GOROOT "%%G"
+
+echo ^> Installing Go tools...
 call go install -v golang.org/x/tools/gopls@latest
 call go install -v mvdan.cc/sh/v3/cmd/shfmt@latest
 call go install -v honnef.co/go/tools/cmd/staticcheck@latest
