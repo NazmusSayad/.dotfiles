450c2d7ee49ea0f05bcc7b331f5a52e7cc2a01cf
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-25 01:41:47 +0600

    Update __install-config.cmd: streamline git configuration by removing duplicate entries and maintaining essential settings for improved setup efficiency.

--- __install-config.cmd
+++ __install-config.cmd
@@ -5,18 +5,13 @@ echo ^> Setting up git config...
 git config --global user.name "Nazmus Sayad"
 git config --global user.email "87106526+NazmusSayad@users.noreply.github.com"
 git config --global init.defaultBranch main
+git config --global --add safe.directory "*"
+git config --global --add --bool push.autoSetupRemote true
 git config --global core.eol lf
 git config --global core.autocrlf false
 git config --global core.pager cat
 git config --global core.ignorecase false
-git config --global --add safe.directory "*"
-git config --global --add --bool push.autoSetupRemote true
-
-echo ^> pnpm config settings...
-call pnpm config set ci true
-call pnpm config set allow-scripts true
-call pnpm config set shamefully-hoist true
-call pnpm config set auto-install-peers true
+git config --global core.editor "code --wait"
 
 echo ^> Symlinking
 call symlink-setup.exe
