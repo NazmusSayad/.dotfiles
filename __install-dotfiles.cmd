e3c4fd7ffc61c32ac2c8e21b4d24b7868fe99b35
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2025-12-28 02:28:19 +0600

    Enhance __install-dotfiles.cmd to check and update the user PATH variable, ensuring DOTFILES_DIR_BIN is included. This improves the installation process by verifying the environment setup and providing feedback on the PATH status.

--- __install-dotfiles.cmd
+++ __install-dotfiles.cmd
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 set "CURRENT_DIR=%~dp0"
 set "CURRENT_DIR=%CURRENT_DIR:~0,-1%"
@@ -10,3 +10,17 @@ set "DOTFILES_DIR_BIN=%DOTFILES_DIR%\.build\bin"
 echo Setting DOTFILES_DIR...
 powershell -NoProfile -Command "[Environment]::SetEnvironmentVariable('DOTFILES_DIR','%DOTFILES_DIR%','User')"
 
+set "USER_PATH="
+for /f "usebackq delims=" %%A in (`powershell -NoProfile -Command "[Environment]::GetEnvironmentVariable('PATH','User')"`) do set "USER_PATH=%%A"
+
+echo %USER_PATH% | find /I "%DOTFILES_DIR_BIN%" >nul 2>&1
+if errorlevel 1 (
+  echo DOTFILES_DIR_BIN not in PATH
+  echo Adding DOTFILES_DIR_BIN to PATH...
+  powershell -NoProfile -Command "[Environment]::SetEnvironmentVariable('PATH','%DOTFILES_DIR_BIN%;%USER_PATH%','User')"
+) else (
+  echo DOTFILES_DIR_BIN already in PATH
+)
+
+echo.
+pause
