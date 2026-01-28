9cf3ead96973c64bc6a1e8fe76eb2fcea02f75fd
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-01-28 12:21:17 +0600

    Add Git credential helper configuration: Set up credential manager for improved authentication in __install-config.cmd.

--- __install-config.cmd
+++ __install-config.cmd
@@ -4,6 +4,7 @@ setlocal
 echo Setting up git config...
 git config --global user.name "Nazmus Sayad"
 git config --global user.email "87106526+NazmusSayad@users.noreply.github.com"
+git config --global credential.helper manager
 git config --global init.defaultBranch main
 git config --global --add safe.directory "*"
 git config --global --add --bool push.autoSetupRemote true
