dc4e8c5045fd15e12e80e4256d5562f1b9346bac
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-06 10:39:56 +0600

    Add fix-ports alias to alias.sh: Introduce a new shell alias for stopping the winnat service, enhancing network configuration management.

--- config/shell/alias.sh
+++ config/shell/alias.sh
@@ -3,17 +3,34 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+# npm
 alias r="nr"
 alias nid="ni -D"
 
+# Flutter
 alias fl="flutter"
 alias flp="flutter pub"
 alias flr="flutter run"
 alias fle="flutter emulators"
 
+# Docker
 alias d="docker"
 alias dc="docker compose"
 alias dcu="docker compose up -d"
 alias dcd="docker compose down"
 
+# Git
+alias gc="git checkout"
+alias gcn="git checkout -b"
+alias gds="git diff --stat"
+
+# GitHub
+alias ghv="gh repo view"
+alias ghw="gh repo view --web"
+
+# Windows
+alias fs-case="fsutil.exe file setCaseSensitiveInfo . enable recursive"
+alias fix-ports="sudo net stop winnat"
+
+# Tools
 alias oc="opencode"
