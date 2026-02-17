86e669a1444333f5c92f08f29a9d1511052076e7
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-02-17 11:07:30 +0600

    Enhance package management in main.go: add Scoop uninstall and update commands, streamline Pacman commands, and ensure Mise package management is clearly defined for improved clarity and functionality.

--- src/scripts/packages-sync/main.go
+++ src/scripts/packages-sync/main.go
@@ -8,46 +8,56 @@ import (
 )
 
 func main() {
+	// Scoop
+	fmt.Println("✘", aurora.Faint("Uninstalling Scoop Apps..."))
+	runCommand([]string{"scoop-prune"})
+	fmt.Println()
+
+	fmt.Println("△", aurora.Faint("Updating Scoop..."))
+	runCommand([]string{"scoop", "update", "--quiet"})
+	fmt.Println()
+
 	fmt.Println("▼", aurora.Faint("Installing Scoop packages..."))
 	runCommand([]string{"scoop-install"})
 	fmt.Println()
 
-	fmt.Println("▼", aurora.Faint("Installing Mise packages..."))
-	runCommand([]string{"mise", "install"})
+	fmt.Println("△", aurora.Faint("Updating Scoop Apps..."))
+	runCommand([]string{"scoop", "update", "--all", "--quiet"})
 	fmt.Println()
 
+	fmt.Println("✘", aurora.Faint("Cleaning Scoop..."))
+	runCommand([]string{"scoop", "cache", "rm", "*"})
+	fmt.Println()
+
+	// Pacman
 	fmt.Println("◯", aurora.Faint("Preparing Pacman..."))
-	runCommand([]string{"pacman", "--noconfirm", "-Sy"})
+	runCommand([]string{"pacman", "-Sy", "--noconfirm"})
 	fmt.Println()
 
 	fmt.Println("▼", aurora.Faint("Installing Pacman packages..."))
 	runCommand([]string{"msys-install"})
 	fmt.Println()
 
-	fmt.Println("△", aurora.Faint("Updating Scoop..."))
-	runCommand([]string{"scoop", "update"})
+	fmt.Println("△", aurora.Faint("Updating Pacman..."))
+	runCommand([]string{"pacman", "-Su", "--noconfirm"})
+	fmt.Println()
+
+	fmt.Println("✘", aurora.Faint("Cleaning Pacman..."))
+	runCommand([]string{"pacman", "-Scc", "--noconfirm"})
+	fmt.Println()
+
+	// Mise
+	fmt.Println("▼", aurora.Faint("Installing Mise packages..."))
+	runCommand([]string{"mise", "install"})
 	fmt.Println()
 
 	fmt.Println("△", aurora.Faint("Updating Mise..."))
 	runCommand([]string{"mise", "upgrade"})
 	fmt.Println()
 
-	fmt.Println("△", aurora.Faint("Updating Pacman..."))
-	runCommand([]string{"pacman", "--noconfirm", "-Su"})
-	fmt.Println()
-
-	fmt.Println("✘", aurora.Faint("Cleaning Scoop..."))
-	runCommand([]string{"scoop-prune"})
-	runCommand([]string{"scoop", "cache", "rm", "*"})
-	fmt.Println()
-
 	fmt.Println("✘", aurora.Faint("Cleaning Mise..."))
 	runCommand([]string{"mise", "prune", "--yes"})
 	runCommand([]string{"mise", "cache", "clear", "--yes"})
-	fmt.Println()
-
-	fmt.Println("✘", aurora.Faint("Cleaning Pacman..."))
-	runCommand([]string{"pacman", "--noconfirm", "-Scc"})
 	fmt.Println()
 }
 
