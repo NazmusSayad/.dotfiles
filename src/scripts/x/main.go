0fdbd0593a74bc06aaf52fe73ccec538b2134865
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-04-09 07:14:41 +0600

    feat(main): add main.go script for command execution

---
+++ src/scripts/x/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("No command provided")
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+	args := os.Args[2:]
+
+	cmd := exec.Command(name, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		}
+
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
