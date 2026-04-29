78ab3e348ac35705f179d97ec7f560120b8b54a0
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-04-29 20:38:45 +0600

    fix: remove unused pipx:docling configuration from mise.toml and clean up powershell.ps1 script

--- config/shell/powershell.ps1
+++ config/shell/powershell.ps1
@@ -4,6 +4,5 @@ Invoke-Expression (&zoxide init powershell | Out-String)
 if ($PSVersionTable.PSEdition -eq 'Core') {
   Invoke-Expression (&pwshac cd | Out-String)
   Invoke-Expression (&shaka pwsh | Out-String)
-
   Invoke-Expression (&starship init powershell)
 }
