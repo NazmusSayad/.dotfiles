9eb90ea75c328bbfcf5a92e6d0d5c95dd9b7c27a
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-06-03 17:42:53 +0600

    Reorder Invoke-Expression calls in powershell.ps1 for improved clarity

--- config/shell/powershell.ps1
+++ config/shell/powershell.ps1
@@ -1,7 +1,7 @@
-Invoke-Expression (& dotsh pwsh @(mise env -D) | Out-String)
-Invoke-Expression "$(direnv hook pwsh)"
-
 if ($PSVersionTable.PSEdition -eq 'Core') {
+  Invoke-Expression (& dotsh pwsh @(mise env -D) | Out-String)
+  Invoke-Expression "$(direnv hook pwsh)"
+
   Invoke-Expression (&pwshac cd | Out-String)
   Invoke-Expression (&shaka pwsh | Out-String)
   Invoke-Expression (&zoxide init powershell | Out-String)
