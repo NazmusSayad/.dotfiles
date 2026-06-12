d19c70e7392abe78b9413553eba496cb303bc212
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-06-13 04:34:06 +0600

    Refactor git configuration: move settings to separate script for better organization

--- src/etc/install-config.ps1
+++ src/etc/install-config.ps1
@@ -1,17 +1,5 @@
 Write-Host "> Setting up git config..."
-git config --global user.name "Nazmus Sayad"
-git config --global user.email "87106526+NazmusSayad@users.noreply.github.com"
-git config --global credential.helper manager
-git config --global init.defaultBranch main
-git config --global --add safe.directory "*"
-git config --global --add --bool push.autoSetupRemote true
-git config --global core.eol lf
-git config --global core.autocrlf false
-git config --global core.pager cat
-git config --system core.longpaths true
-git config --global core.ignorecase false
-git config --global core.editor "code --wait"
-git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
+bash "./src/shell/git-config.sh"
 
 Write-Host ""
 Write-Host "> Symlinking..."
---
+++ src/shell/git-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git config --global user.name "Nazmus Sayad"
+git config --global user.email "87106526+NazmusSayad@users.noreply.github.com"
+git config --global credential.helper manager
+git config --global init.defaultBranch main
+git config --global --add safe.directory "*"
+git config --global --add --bool push.autoSetupRemote true
+git config --global core.eol lf
+git config --global core.autocrlf false
+git config --global core.pager cat
+git config --system core.longpaths true
+git config --global core.ignorecase false
+git config --global core.editor "code --wait"
+git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
