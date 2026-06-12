4e2e555dc34c54690c2066b025a245479ea94930
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-06-13 05:39:00 +0600

    Add Zsh initialization for direnv, shaka, zoxide, and starship in macOS config

--- config/shell/zsh-config.mac.sh
+++ config/shell/zsh-config.mac.sh
@@ -1,2 +1,9 @@
 eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 eval "$(mise activate zsh)"
+eval "$(direnv hook zsh)"
+
+if [[ $- == *i* ]]; then
+	eval "$(shaka zsh)"
+	eval "$(zoxide init zsh)"
+	eval "$(starship init zsh)"
+fi
