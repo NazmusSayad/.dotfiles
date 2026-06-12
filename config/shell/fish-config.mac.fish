32e152c5b9abd3db76c313bf43c1ef48b027b2ae
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-06-13 05:41:58 +0600

    Add Fish shell configuration for macOS with direnv, shaka, zoxide, and starship initialization

---
+++ config/shell/fish-config.mac.fish
@@ -0,0 +1,22 @@
+mise activate fish | source
+direnv hook fish | source
+
+function fish_greeting
+end
+
+if status is-interactive
+    fish_config theme choose default
+    set fish_color_end normal
+    set fish_color_quote green
+    set fish_color_comment --dim
+    set fish_color_command magenta
+
+    shaka fish | source
+    zoxide init fish | source
+    starship init fish | source
+
+    zoxide add $PWD
+    function on_cd --on-variable PWD
+        zoxide add $PWD
+    end
+end
