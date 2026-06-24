338e26ce67e90ba40fb70ea7d668978e0cdef7bc
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-06-24 20:35:54 +0600

    Enhance macOS installation script and OBS configuration with additional settings

--- __install-mac.sh
+++ __install-mac.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
 sudo spctl --master-disable
-
 sudo mdutil -a -i off
 sudo mdutil -a -E
+
+sudo launchctl disable system/com.apple.assistantd
+
+defaults write com.apple.dock tilesize -int 32
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.1
+defaults write com.apple.dock show-recents -bool false
+
+defaults write com.apple.finder QuitMenuItem -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowRecentTags -bool false
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
