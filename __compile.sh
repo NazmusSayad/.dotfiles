eac0567f38eac097ac5e7635217fb409af326b84
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-02 08:25:52 +0600

    Clean up __compile.sh: remove unnecessary echo statements

--- __compile.sh
+++ __compile.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
+
 set -e
-
-echo "> Cleaning build directory..."
 rm -rf ./.build/bin
-
-echo ""
-echo "> Compiling Go scripts..."
 go run ./src/compile-scripts/main.go
-
-echo ""
-echo "Done!"
