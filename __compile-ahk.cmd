38c313944bfb20f1d655bad62f9617b0711edd42
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-02 08:20:37 +0600

    Refactor compile scripts: remove AHK handling from __compile.sh and create __compile-ahk.cmd

---
+++ __compile-ahk.cmd
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+cd /d "%~dp0"
+
+echo ^> Killing all AHK scripts...
+set "killed="
+for /f "tokens=1" %%p in ('tasklist /fi "IMAGENAME eq AHK-*" /nh 2^>nul ^| findstr /i "AHK-"') do (
+    set "name=%%p"
+    set "name=!name:.exe=!"
+    set "killed=!killed! !name!"
+)
+taskkill /F /IM "AHK-*" >nul 2>&1
+
+echo.
+echo ^> Cleaning AHK build directory...
+if exist ".build\ahk" rmdir /s /q ".build\ahk"
+
+echo.
+echo ^> Compiling AutoHotkey scripts...
+call go run ./src/compile-ahk/main.go
+
+echo.
+echo ^> Restarting AHK scripts...
+for %%n in (%killed%) do (
+    set "exe=%CD%\.build\ahk\%%n.exe"
+    if exist "!exe!" (
+        echo ^> Restarting: !exe!
+        powershell -NoProfile -Command "Start-Process '!exe!' -Verb RunAs"
+    )
+)
+
+echo.
+echo Done!
