a6daba4704b6b1a071b7f1c25e1e6674c00067cc
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-02 08:04:06 +0600

    Refactor fish configuration: streamline greeting function and enhance theme settings

--- config/shell/fish-config.fish
+++ config/shell/fish-config.fish
@@ -1,29 +1,31 @@
+function fish_greeting
+end
+
+fish_config theme choose default
+set fish_color_end normal
+set fish_color_quote green
+set fish_color_comment --dim
+set fish_color_command magenta
+
+# Mise
 if test "$OS" = Windows_NT
     dotsh fish (mise env -D) | source
 else
     mise activate fish | source
 end
 
+# Environment
 test -f ~/.env; and dotsh fish "$(string collect < ~/.env)" | source
 test -f ~/.path; and set -x PATH $PATH (cat ~/.path)
 direnv hook fish | source
 
-function fish_greeting
-end
+# Enhancements
+shaka fish | source
+zoxide init fish | source
+starship init fish | source
 
-if status is-interactive
-    fish_config theme choose default
-    set fish_color_end normal
-    set fish_color_quote green
-    set fish_color_comment --dim
-    set fish_color_command magenta
-
-    shaka fish | source
-    zoxide init fish | source
-    starship init fish | source
-
+# zoxide
+zoxide add $PWD
+function on_cd --on-variable PWD
     zoxide add $PWD
-    function on_cd --on-variable PWD
-        zoxide add $PWD
-    end
 end
