b823e7a2594789a543bd18c63125fd1b6d8880f7
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-04 03:58:36 +0600

    Add NSScrollViewRubberbanding setting to disable rubberbanding effect

--- __setup-macos.sh
+++ __setup-macos.sh
@@ -73,6 +73,7 @@ defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
 defaults write NSGlobalDomain CGDisableCursorLocationMagnification -bool true
 defaults write NSGlobalDomain NSScrollViewRubberbanding -bool false
+defaults write NSScrollViewRubberbanding -int 0
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 defaults write NSGlobalDomain AppleLiveTextEnabled -bool false
