52dd76f147790dce64dea00adca26f5d7480137c
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-03 14:04:17 +0600

    Add warning message to Windows setup script for clarity

--- __setup-windows.cmd
+++ __setup-windows.cmd
@@ -1,3 +1,5 @@
+:: WARNING: THIS IS ONE TIME SETUP SCRIPT FOR WINDOWS
+
 @echo off
 powershell -Command "if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) { exit 1 }"
 if %errorLevel% NEQ 0 (
