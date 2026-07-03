8104db50351048bfe16a4121da7054d610f4f41e
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-04 04:18:42 +0600

    Fix bash-profile.sh to correct PATH export order for environment variables

--- config/shell/bash-profile.sh
+++ config/shell/bash-profile.sh
@@ -2,7 +2,7 @@ eval "$(/opt/homebrew/bin/brew shellenv bash)"
 eval "$(mise env --shell bash)"
 
 [[ -f ~/.env ]] && eval "$(dotsh bash "$(cat ~/.env)")"
-[[ -f ~/.path ]] && export PATH="$(paste -sd ':' ~/.path):$PATH"
+[[ -f ~/.path ]] && export PATH="$PATH:$(paste -sd ':' ~/.path)"
 
 export RUST_BACKTRACE=1
 export NODE_NO_WARNINGS=1
