7f7182cd4e0eb3eeea372ef1e37010c76884d9e1
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-04 04:17:02 +0600

    Refactor bash-profile.sh to simplify conditional checks for environment files

--- config/shell/bash-profile.sh
+++ config/shell/bash-profile.sh
@@ -1,13 +1,8 @@
 eval "$(/opt/homebrew/bin/brew shellenv bash)"
 eval "$(mise env --shell bash)"
 
-if [[ -f ~/.env ]]; then
-	eval "$(dotsh bash "$(cat ~/.env)")"
-fi
-
-if [[ -f ~/.path ]]; then
-	export PATH="$(paste -s -d ':' ~/.path):$PATH"
-fi
+[[ -f ~/.env ]] && eval "$(dotsh bash "$(cat ~/.env)")"
+[[ -f ~/.path ]] && export PATH="$(paste -sd ':' ~/.path):$PATH"
 
 export RUST_BACKTRACE=1
 export NODE_NO_WARNINGS=1
