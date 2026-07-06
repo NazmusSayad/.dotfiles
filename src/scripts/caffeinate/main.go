900e8768f4a8f0a80558902ec5394f3af919504f
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-07-07 05:24:52 +0600

    Refactor caffeinate script to include periodic status updates and graceful shutdown

--- src/scripts/caffeinate/main.go
+++ src/scripts/caffeinate/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 const (
@@ -20,9 +22,24 @@ func main() {
 		uintptr(ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED),
 	)
 
+	start := time.Now()
+	fmt.Printf("[%s] caffeinate on: sleep and display blocked\n", start.Format("15:04:05"))
+	fmt.Println("press Ctrl+C to stop")
+
+	ticker := time.NewTicker(time.Minute)
+	defer ticker.Stop()
+
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-	<-sig
 
-	setThreadExecutionState.Call(uintptr(ES_CONTINUOUS))
+	for {
+		select {
+		case now := <-ticker.C:
+			fmt.Printf("[%s] awake for %s\n", now.Format("15:04:05"), now.Sub(start).Round(time.Second))
+		case <-sig:
+			setThreadExecutionState.Call(uintptr(ES_CONTINUOUS))
+			fmt.Printf("[%s] caffeinate off: sleep restored after %s\n", time.Now().Format("15:04:05"), time.Since(start).Round(time.Second))
+			return
+		}
+	}
 }
