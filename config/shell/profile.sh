8843ab9fe3e7b31962207277311bcc105b0eabe5
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-08-01 11:48:36 +0600

    Update profile configuration: remove redundant environment variable exports

--- config/shell/profile.sh
+++ config/shell/profile.sh
@@ -4,8 +4,3 @@ eval "$(/opt/homebrew/bin/brew shellenv bash)"
 eval "$(mise env --shell bash)"
 
 [[ -f ~/.env ]] && eval "$(dotsh bash "$(cat ~/.env)")"
-
-export RUST_BACKTRACE=1
-export NODE_NO_WARNINGS=1
-
-export OPENCODE_SHELL="/opt/homebrew/bin/bash"
