7e1a065d40fcc383e554f35bad6117f9c1e6b303
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-08-01 11:45:40 +0600

    Update profile configuration: correct OPENCODE_SHELL export and reorder environment variable exports

--- config/shell/profile.sh
+++ config/shell/profile.sh
@@ -5,7 +5,7 @@ eval "$(mise env --shell bash)"
 
 [[ -f ~/.env ]] && eval "$(dotsh bash "$(cat ~/.env)")"
 
-export OPENCODE_SHELL="$(which bash)"
-
-export NODE_NO_WARNINGS=1
 export RUST_BACKTRACE=1
+export NODE_NO_WARNINGS=1
+
+export OPENCODE_SHELL="/opt/homebrew/bin/bash"
