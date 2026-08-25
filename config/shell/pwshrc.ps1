ca3b142d49afa54b0dc4cf6d7dd02d96f97d81f4
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-08-25 06:48:21 +0600

    fix(pwshrc): remove unnecessary Where-Object calls for cleaner execution

--- config/shell/pwshrc.ps1
+++ config/shell/pwshrc.ps1
@@ -8,15 +8,15 @@ if (Test-Path ~/.path) {
     }
 }
 
-Invoke-Expression (pwshac cd ls cp mv rm cat echo pwd man kill sleep ps history clear | Out-String -Width ([int]::MaxValue))
+pwshac | Out-String -Width ([int]::MaxValue) | Invoke-Expression
 
-dotsh pwsh (mise env --dotenv) | Out-String -Width ([int]::MaxValue) | Where-Object { $_.Trim() } | Invoke-Expression
+dotsh pwsh (mise env --dotenv) | Out-String -Width ([int]::MaxValue) | Invoke-Expression
 
-if (Test-Path ~/.dotfiles/.env) { dotsh pwsh (Get-Content ~/.dotfiles/.env -Raw) | Out-String -Width ([int]::MaxValue) | Where-Object { $_.Trim() } | Invoke-Expression }
-if (Test-Path ~/.env) { dotsh pwsh (Get-Content ~/.env -Raw) | Out-String -Width ([int]::MaxValue) | Where-Object { $_.Trim() } | Invoke-Expression }
+if (Test-Path ~/.dotfiles/.env) { dotsh pwsh (Get-Content ~/.dotfiles/.env -Raw) | Out-String -Width ([int]::MaxValue) | Invoke-Expression }
+if (Test-Path ~/.env) { dotsh pwsh (Get-Content ~/.env -Raw) | Out-String -Width ([int]::MaxValue) | Invoke-Expression }
 
-direnv hook pwsh | Out-String -Width ([int]::MaxValue) | Where-Object { $_.Trim() } | Invoke-Expression
+direnv hook pwsh | Out-String -Width ([int]::MaxValue) | Invoke-Expression
 
-shaka pwsh | Out-String -Width ([int]::MaxValue) | Where-Object { $_.Trim() } | Invoke-Expression
-zoxide init powershell | Out-String -Width ([int]::MaxValue) | Where-Object { $_.Trim() } | Invoke-Expression
-starship init powershell | Out-String -Width ([int]::MaxValue) | Where-Object { $_.Trim() } | Invoke-Expression
+shaka pwsh | Out-String -Width ([int]::MaxValue) | Invoke-Expression
+zoxide init powershell | Out-String -Width ([int]::MaxValue) | Invoke-Expression
+starship init powershell | Out-String -Width ([int]::MaxValue) | Invoke-Expression
