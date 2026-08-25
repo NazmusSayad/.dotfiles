15da703cc66c3f8522b1c7261f74043d6ff883ad
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2026-08-26 04:39:31 +0600

    chore(git-config): enable core.symlinks

--- etc/git-config.sh
+++ etc/git-config.sh
@@ -3,14 +3,19 @@
 echo "Configuring git..."
 git config --global user.name "Nazmus Sayad"
 git config --global user.email "87106526+NazmusSayad@users.noreply.github.com"
-git config --global init.defaultBranch main
 git config --global --add safe.directory "*"
 git config --global --add --bool push.autoSetupRemote true
+
 git config --global core.eol lf
 git config --global core.autocrlf false
 git config --global core.pager cat
+
+git config --global init.defaultBranch main
 git config --global pull.rebase false
+
+git config --global core.symlinks true
 git config --global core.longpaths true
 git config --global core.ignorecase false
+
 git config --global core.editor "code --wait"
 git config --global core.excludesfile ~/.gitignore
